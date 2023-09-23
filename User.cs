a0dfe65dd4661516d9c0a64dcaf37aa2f2169026
Vovka88 <reddedkord@gmail.com>
2023-09-23 20:27:01 +0300

    Класс User:
    Параметри:
    Name - Ім'я людини;
    Age - Її вік;
    COfB - Країна народження;
    CurRes - Місто проживання;
    PrevRes - Минуло місто Проживання;
    Language - Мова спілкування;
    
    Методи:
    string ToString - Метод який виводить УСЮ інформацію про користувача;
    string ToStringBaseInfo - Метод який виводить БАЗОВУ інформацію про користувача ( Ім'я, Вік, Країна народження );
    string ToStringAdditionalInfo - Метод який виводить ДОДАТКОВУ інформацію про користувача;

---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿namespace UnsafeCode_TeamPlay
+{
+    public class User
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string COfB { get; set; }
+        public string CurRes { get; set; }
+        public string PrevRes { get; set; }
+        public string Language { get; set; }
+
+        public User(string Name, int Age, string COfB, string CurRes, string PrevRes, string Language) {
+            this.Name = Name;
+            this.Age = Age;
+            this.COfB = COfB;
+            this.CurRes = CurRes;
+            this.PrevRes = PrevRes;
+            this.Language = Language;
+        }
+
+        public override string ToString() { return $"Name: {Name}\nAge: {Age}\nCountry of Birthday: {COfB}\nLanguage: {Language}\nCurrent Residence: {CurRes}\nPrevious Residence: {PrevRes}  "; }
+        public string ToStringAdditionalInfo() { return $"Language: {Language}\nCurrent Residence: {CurRes}\nPrevious Residence: {PrevRes}  "; }
+        public string ToStringBaseInfo() { return $"Name: {Name}\nAge: {Age}\nCountry of Birthday: {COfB}"; }
+    }
+}
